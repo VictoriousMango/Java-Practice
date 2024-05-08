374859a9d1faf38bb842fc9c0ead85aef6ae9960
Additya Akash Mishra <77526069+VictoriousMango@users.noreply.github.com>
2024-05-08 20:02:13 +0000

    Scanning with Java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 class Main {
   public static void main(String args[]){
-    int rowNumbers = 4;
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the number of row: ");
+    int rowNumbers = sc.nextInt();
     for (int i = 0; i < rowNumbers; i++){
       for (int j = 0; j < i+1; j++){
         System.out.print("*");
