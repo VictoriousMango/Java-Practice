3f56c113f6d1db5eec6d3251deb4f20fb18a817c
Additya Akash Mishra <77526069+VictoriousMango@users.noreply.github.com>
2024-05-08 19:38:53 +0000

    Trying Pattern with for Loop

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 class Main {
   public static void main(String args[]){
-    System.out.println("Hello World !!!");
+    int rowNumbers = 4;
+    for (int i = 0; i < rowNumbers; i++){
+      for (int j = 0; j < i+1; j++){
+        System.out.print("*");
+      }
+      System.out.println();
+    }
   }
 }
